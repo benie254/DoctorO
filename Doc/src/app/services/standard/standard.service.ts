d3334ca753fe43efb83e549fb87c891a91bff82a
benie254 <davinci.monalissa@gmail.com>
2023-07-18 15:31:51 +0300

    update api url

--- Doc/src/app/services/standard/standard.service.ts
+++ Doc/src/app/services/standard/standard.service.ts
@@ -4,8 +4,8 @@ import { Observable } from 'rxjs';
 import { Contact } from 'src/app/classes/contact/contact';
 import { ReqHandlerService } from 'src/app/helpers/requests/req-handlers.service';
 
-// const api = 'https://doctoro-api-production-b463.up.railway.app/api/';
-const api = 'http://127.0.0.1:8000/api/';
+const api = 'https://doctoro-api-production.up.railway.app/api/';
+// const api = 'http://127.0.0.1:8000/api/';
 
 @Injectable({
   providedIn: 'root'
